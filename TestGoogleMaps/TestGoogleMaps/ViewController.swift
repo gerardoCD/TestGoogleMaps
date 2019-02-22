351059121bc83b2e93eb7405b99aac79ffa809c9
Gerardo <ferrari_gerardo@live.com.mx>
2019-02-21 23:15:27 -0600

    Project Empty

---
+++ TestGoogleMaps/TestGoogleMaps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGoogleMaps
+//
+//  Created by Gerardo Castillo Duran  on 2/21/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
